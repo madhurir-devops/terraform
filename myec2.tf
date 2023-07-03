cab09285fc897f2ab6bd118e7ec674a169903a70
madhurir-devops <129939670+madhurir-devops@users.noreply.github.com>
2023-07-03 08:35:47 +0530

    Create myec2.tf

---
+++ myec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "first_ec2_instance" {
+  ami           = "ami-03b31136fc503b84a"
+  instance_type = var.instance_type
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
